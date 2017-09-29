323a8e5bad707c5f54f4681824ce081828753b85
ng1234 <32106505+ng1234@users.noreply.github.com>
2017-09-29 15:57:56 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Enter your car's MPG rating (miles/gallon) as a positive integer: ");
+
+
+    }
+}
+
+
+
